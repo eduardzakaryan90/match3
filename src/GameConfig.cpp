4e1d576215a8bb6cc9cff57a3e36b2d6c248290a
Eduard <eduardzakaryan90@gmail.com>
2020-07-26 01:13:48 +0400

    Added GameConfig class for reading and validating config.json file

---
+++ src/GameConfig.cpp
@@ -0,0 +1,200 @@
+#include "GameConfig.h"
+
+#include "rapidjson/document.h"
+
+#include <fstream>
+#include <experimental/filesystem>
+
+namespace match3
+{
+	bool GameConfig::readJsonFile()
+	{
+		std::string path =  std::experimental::filesystem::current_path().append(JSON_FILE_NAME).string();
+		std::ifstream jsonFile(path);
+		if (!jsonFile.is_open()) {
+			return false;
+		}
+
+		std::ostringstream ss;
+		ss << jsonFile.rdbuf();
+		
+		rapidjson::Document root;
+		root.Parse(ss.str().c_str());
+		if (root.IsNull() || !root.IsObject()) {
+			return false;
+		}
+
+
+		const char* enableBloackFiguresFieldName = ENABLE_BLOCK_FIGURES_FIELD_NAME.c_str();
+		if (!root.HasMember(enableBloackFiguresFieldName)) {
+			throw MissingConfigJsonFieldException(ENABLE_BLOCK_FIGURES_FIELD_NAME);
+		}
+		rapidjson::Value& enableBloackFigures = root[enableBloackFiguresFieldName];
+		if (enableBloackFigures.IsNull() || !enableBloackFigures.IsBool()) {
+			throw MissingConfigJsonFieldException(ENABLE_BLOCK_FIGURES_FIELD_NAME);
+		}
+		setEnableBlockFigures(enableBloackFigures.GetBool());
+
+
+		const char* boardWidthFieldName = BOARD_COLUMNS_FIELD_NAME.c_str();
+		if (!root.HasMember(boardWidthFieldName)) {
+			throw MissingConfigJsonFieldException(BOARD_COLUMNS_FIELD_NAME);
+		}
+		rapidjson::Value& boardWidth = root[boardWidthFieldName];
+		if (boardWidth.IsNull() || !boardWidth.IsInt()) {
+			throw MissingConfigJsonFieldException(BOARD_COLUMNS_FIELD_NAME);
+		}
+		setBoardColumns(boardWidth.GetInt());
+
+
+		const char* boardHeightFieldName = BOARD_ROWS_FIELD_NAME.c_str();
+		if (!root.HasMember(boardHeightFieldName)) {
+			throw MissingConfigJsonFieldException(BOARD_ROWS_FIELD_NAME);
+		}
+		rapidjson::Value& boardHeight = root[boardHeightFieldName];
+		if (boardHeight.IsNull() || !boardHeight.IsInt()) {
+			throw MissingConfigJsonFieldException(BOARD_ROWS_FIELD_NAME);
+		}
+		setBoardRows(boardHeight.GetInt());
+
+
+		const char* movesCountFieldName = MOVES_COUNT_FIELD_NAME.c_str();
+		if (!root.HasMember(movesCountFieldName)) {
+			throw MissingConfigJsonFieldException(MOVES_COUNT_FIELD_NAME);
+		}
+		rapidjson::Value& movesCount = root[movesCountFieldName];
+		if (movesCount.IsNull() || !movesCount.IsInt()) {
+			throw MissingConfigJsonFieldException(MOVES_COUNT_FIELD_NAME);
+		}
+		setMovesCount(movesCount.GetInt());
+
+
+		const char* figuresFieldName = FIGURE_LIST_FIELD_NAME.c_str();
+		if (!root.HasMember(figuresFieldName)) {
+			throw MissingConfigJsonFieldException(FIGURE_LIST_FIELD_NAME);
+		}
+		rapidjson::Value& figures = root[figuresFieldName];
+		if (figures.IsNull() || !figures.IsArray()) {
+			throw MissingConfigJsonFieldException(FIGURE_LIST_FIELD_NAME);
+		}
+
+
+		rapidjson::Document::Array& figuresArrayObj = figures.GetArray();
+		for (rapidjson::SizeType i = 0; i < figuresArrayObj.Size(); i++) {
+			rapidjson::Value& figure = figuresArrayObj[i];
+			if (figure.IsNull() || !figure.IsObject()) {
+				continue;
+			}
+
+			++m_figuresSet;
+
+			const char* colorFieldName = FIGURE_COLOR_FIELD_NAME.c_str();
+			if (!figure.HasMember(colorFieldName)) {
+				throw MissingConfigJsonFieldException(FIGURE_COLOR_FIELD_NAME);
+			}
+			rapidjson::Value& colorName = figure[colorFieldName];
+			if (colorName.IsNull() || !colorName.IsString()) {
+				throw MissingConfigJsonFieldException(FIGURE_COLOR_FIELD_NAME);
+			}
+			std::string color = colorName.GetString();
+
+			int objectiveValue = 0;
+			const char* objectiveFieldName = FIGURE_OBJECTIVE_FIELD_NAME.c_str();
+			if (figure.HasMember(objectiveFieldName)) {
+				rapidjson::Value& objective = figure[objectiveFieldName];
+				if (objective.IsNull() || !objective.IsInt()) {
+					throw MissingConfigJsonFieldException(FIGURE_OBJECTIVE_FIELD_NAME);
+				}
+				objectiveValue = objective.GetInt();
+				++m_objectivesSet;
+			}
+
+			addToFiguresConfig(color, objectiveValue);
+		}
+
+
+		if (m_figuresSet < MIN_FIGURES_COUNT || m_figuresSet > MAX_FIGURES_COUNT) {
+			throw WrongConfigJsonRuleException(FIGURE_LIST_FIELD_NAME,
+				MIN_FIGURES_COUNT, MAX_FIGURES_COUNT, m_figuresSet);
+		}
+		if (m_objectivesSet < MIN_OBJECTIVES_COUNT || m_objectivesSet > MAX_OBJECTIVES_COUNT) {
+			throw WrongConfigJsonRuleException(FIGURE_OBJECTIVE_FIELD_NAME,
+				MIN_OBJECTIVES_COUNT, MAX_OBJECTIVES_COUNT, m_objectivesSet);
+		}
+
+		return true;
+	}
+
+	void GameConfig::setEnableBlockFigures(const bool enable)
+	{
+		m_enableBlockFigures = enable;
+	}
+
+	void GameConfig::setBoardColumns(const int width)
+	{
+		if (width < MIN_COLUMNS || width > MAX_COLUMNS) {
+			throw WrongConfigJsonRuleException(BOARD_COLUMNS_FIELD_NAME,
+				MIN_COLUMNS, MAX_COLUMNS, width);
+		}
+
+		m_boardColumns = width;
+	}
+
+	void GameConfig::setBoardRows(const int height)
+	{
+		if (height < MIN_ROWS || height > MAX_ROWS) {
+			throw WrongConfigJsonRuleException(BOARD_ROWS_FIELD_NAME,
+				MIN_ROWS, MAX_ROWS, height);
+		}
+
+		m_boardRows = height;
+	}
+
+	void GameConfig::setMovesCount(const int movesCount)
+	{
+		if (movesCount < MIN_MOVES_COUNT || movesCount > MAX_MOVES_COUNT) {
+			throw WrongConfigJsonRuleException(MOVES_COUNT_FIELD_NAME,
+				MIN_MOVES_COUNT, MAX_MOVES_COUNT, movesCount);
+		}
+
+		m_movesCount = movesCount;
+	}
+
+	void GameConfig::addToFiguresConfig(std::string colorName, int objective)
+	{
+		if (COLORS.erase(colorName) == 0) {
+			throw InvalidOrDuplicateColorException(colorName);
+		}
+		
+		if (objective < MIN_OBJECTIVE_TARGET || objective > MAX_OBJECTIVE_TARGET) {
+			throw WrongConfigJsonRuleException(FIGURE_OBJECTIVE_FIELD_NAME,
+				MIN_OBJECTIVE_TARGET, MAX_OBJECTIVE_TARGET, objective);
+		}
+
+		m_figuresConfig.push_back(std::pair<std::string, int>(colorName, objective));
+	}
+
+	bool GameConfig::getEnableBlockFigures() const
+	{
+		return m_enableBlockFigures;
+	}
+
+	int GameConfig::getBoardColumns() const
+	{
+		return m_boardColumns;
+	}
+
+	int GameConfig::getBoardRows() const
+	{
+		return m_boardRows;
+	}
+
+	int GameConfig::getMovesCount() const
+	{
+		return m_movesCount;
+	}
+	std::list<std::pair<std::string, int>> GameConfig::getFiguresConfig() const
+	{
+		return m_figuresConfig;
+	}
+}
---
+++ src/GameConfig.h
@@ -0,0 +1,144 @@
+#pragma once
+
+#include <sstream>
+#include <string>
+#include <list>
+#include <set>
+
+namespace match3
+{
+	class GameConfig
+	{
+	private:
+		const int MIN_COLUMNS = 7;
+		const int MAX_COLUMNS = 10;
+		const int MIN_ROWS = 7;
+		const int MAX_ROWS = 10;
+		const int MIN_MOVES_COUNT = 1;
+		const int MAX_MOVES_COUNT = 999;
+		const int MIN_OBJECTIVE_TARGET = 1;
+		const int MAX_OBJECTIVE_TARGET = 999;
+		const int MIN_FIGURES_COUNT = 3;
+		const int MAX_FIGURES_COUNT = 5;
+		const int MIN_OBJECTIVES_COUNT = 1;
+		const int MAX_OBJECTIVES_COUNT = 3;
+
+		const std::string ENABLE_BLOCK_FIGURES_FIELD_NAME = "enableBloackFigures";
+		const std::string BOARD_COLUMNS_FIELD_NAME = "boardColumns";
+		const std::string BOARD_ROWS_FIELD_NAME = "boardRows";
+		const std::string MOVES_COUNT_FIELD_NAME = "movesCount";
+		const std::string FIGURE_LIST_FIELD_NAME = "figures";
+		const std::string FIGURE_COLOR_FIELD_NAME = "color";
+		const std::string FIGURE_OBJECTIVE_FIELD_NAME = "objective";
+
+		const std::string JSON_FILE_NAME = "config.json";
+
+		std::set<std::string> COLORS{ "red", "green", "blue", "orange", "violet" };
+
+	public:
+		bool readJsonFile();
+
+		// setters
+		void setEnableBlockFigures(const bool enable);
+		void setBoardColumns(const int width);
+		void setBoardRows(const int height);
+		void setMovesCount(const int movesCount);
+		void addToFiguresConfig(std::string colorName, int objective);
+
+		// getters
+		bool getEnableBlockFigures() const;
+		int getBoardColumns() const;
+		int getBoardRows() const;
+		int getMovesCount() const;
+		std::list<std::pair<std::string, int>> getFiguresConfig() const;
+
+	private:
+		bool m_enableBlockFigures;
+		int m_boardColumns;
+		int m_boardRows;
+		int m_movesCount;
+		std::list<std::pair<std::string, int>> m_figuresConfig; // key - figure color name, value - objective. If value is 0 - no objective set.
+
+		int m_figuresSet = 0;
+		int m_objectivesSet = 0;
+	};
+
+	struct WrongConfigJsonRuleException : public std::exception
+	{
+		const std::string m_field;
+		const int m_minValue;
+		const int m_maxValue;
+		const int m_actualValue;
+
+		mutable std::string errorString;
+
+		explicit WrongConfigJsonRuleException(const std::string field, const int minValue, const int maxValue, const int actualValue)
+			: std::exception()
+			, m_field(field)
+			, m_minValue(minValue)
+			, m_maxValue(maxValue)
+			, m_actualValue(actualValue)
+		{
+
+		}
+
+		virtual ~WrongConfigJsonRuleException() throw () override {}
+
+		virtual const char * what() const throw () override
+		{
+			std::stringstream ss;
+			ss << "Out of config JSON rules, field name: " << m_field << ". Min value is: " << m_minValue
+				<< ", max value is: " << m_maxValue << ", but actual value was: " << m_actualValue;
+			errorString = ss.str();
+			return errorString.c_str();
+		}
+	};
+
+	struct MissingConfigJsonFieldException : public std::exception
+	{
+		const std::string m_field;
+
+		mutable std::string errorString;
+
+		explicit MissingConfigJsonFieldException(const std::string field)
+			: std::exception()
+			, m_field(field)
+		{
+
+		}
+
+		virtual ~MissingConfigJsonFieldException() throw () override {}
+
+		virtual const char * what() const throw () override
+		{
+			std::stringstream ss;
+			ss << "Field is missing or has wrong type, field name: " << m_field;
+			errorString = ss.str();
+			return errorString.c_str();
+		}
+	};
+
+	struct InvalidOrDuplicateColorException : public std::exception
+	{
+		const std::string m_colorName;
+
+		mutable std::string errorString;
+
+		explicit InvalidOrDuplicateColorException(const std::string colorName)
+			: std::exception()
+			, m_colorName(colorName)
+		{
+
+		}
+
+		virtual ~InvalidOrDuplicateColorException() throw () override {}
+
+		virtual const char * what() const throw () override
+		{
+			std::stringstream ss;
+			ss << "Invalid or duplicate color name: " << m_colorName;
+			errorString = ss.str();
+			return errorString.c_str();
+		}
+	};
+}
